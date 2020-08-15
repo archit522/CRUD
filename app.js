a3881d2c34d6e2e86d831208995b01b59133c828
Archit Aggarwal <archit522@gmail.com>
2020-08-15 15:10:07 +0530

    Adding comments and removing dead code

--- app.js
+++ app.js
@@ -6,34 +6,35 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
+//Using Redis client to setup connection to local redis server cache
 var redisClient = require('redis').createClient;
 var redis = redisClient(6379, 'localhost');
 
+//.env file contains database link URL
 require('dotenv/config')
-// //MIDDLEWARES
-// app.use('/posts', () => {
-// 	console.log('This is a middleware running');
-// });
 
 app.use(bodyParser.json());
 
-//Import Routes
+//Importing Routes to handle API calls
 const postsRoute = require('./routes/posts');
 const jobsRoute = require('./routes/jobs');
-//ROUTES
+
+//Defining Routes
 app.get('/', (req, res) =>{
 	res.send('We are on home');
 });
 
 app.use('/posts', postsRoute);
 app.use('/jobs', jobsRoute);
-//Connect to DB
+
+//Check connection to MongoDB
 mongoose.connect(process.env.DB_CONNECTION, 
 	{ useNewUrlParser: true, useUnifiedTopology: true}, 
 	() =>{
 	console.log('connected to DB!')
 });
 
+//Check connection to Redis Cache
 redis.on("connect", () => {
     console.log('connected to Redis');
 });
